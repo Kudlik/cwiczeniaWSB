1f199464e92999a7241b467c70ed9321a772b877
Kudlik <jaskulski.maciek@gmail.com>
2017-04-22 14:23:28 +0200

    6

--- danetestowe.py
+++ danetestowe.py
@@ -4,4 +4,4 @@ USER = ['tomasz.rosenberg'] #login do poczty
 PSWD = ['qazwsxedcr'] #hasło do poczty
 RECIPIENT = ['tomasz.rosenberg@gmail.com'] #odbiorca wiadomości
 TITLE = ['mail testowy robotowy'] #tytuł maila
-CONTENT = ['test pierwszy, siemanko!'] #treść maila
+CONTENT = ['siemanko!'] #treść maila
